13c039f4a34d4bdefeedb71e63ecb06ab90ca7c1
rachid <rachidglobalia@gmail.com>
2021-03-04 20:47:39 +0100

    mod

--- admin.php
+++ admin.php
@@ -1,3 +1,21 @@
+<html>
+<head>
+  <title>USERS</title>
+</head>
+<body>
+  <table border="10" align="center">
+
+    <tr>
+
+      <th>Nom</th>
+
+      <th>Email</th>
+
+      <th>Pasword</th>
+
+      <th>Rol</th>
+
+    </tr>
 <?php
 $db = new mysqli('10.100.66.90','phpmyadmin','1234','users');
 
@@ -10,6 +28,10 @@ if ($db->connect_error) {
 $sql = "SELECT * FROM users";
 $resultat = $db->query($sql);
 while($row = $resultat->fetch_assoc()) {
-         echo "Username: " . $row["username"]. " Email: " . $row["email"]. " Password: " . $row["password"]. "<br>";
+      echo "<tr><td>" . $row["username"]. "</td>"
+           "<td>" . $row["email"]."</td>"
+           "<td>" . $row["password"]. "</tr></td><br>";
 }
 ?>
+</body>
+<html>
