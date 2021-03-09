52506c59567a683d2d47b4599fe5d4f6c40c0d2f
rachid <rachidglobalia@gmail.com>
2021-03-09 19:22:04 +0100

    mod

---
+++ updateform.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2>Create Result</h2>
+<!--we are using GET method to see de parameters in the url-->
+<form action="updateadmin.php" method="GET">
+<p>USERNAME: <input type="text" name="username"></p>
+<p>EMAIL: <input type="text" name="email"></p>
+<p>PASSWORD: <input type="text" name="password"></p>
+<p>ROL: <input type="text" name="rol"></p>
+<input type="submit">
+</form>
+</body>
+</html>
