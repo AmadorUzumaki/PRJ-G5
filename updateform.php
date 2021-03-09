13dee84e9794afc7a3808115a5793724aeb554d1
AmadorUzumaki <amadorherediaroldan@gmail.com>
2021-03-09 20:22:46 +0100

    mod

--- updateform.php
+++ updateform.php
@@ -3,10 +3,10 @@
 <h2>Create Result</h2>
 <!--we are using GET method to see de parameters in the url-->
 <form action="updateadmin.php" method="GET">
-<p>USERNAME: <input type="text" name="username"></p>
-<p>EMAIL: <input type="text" name="email"></p>
-<p>PASSWORD: <input type="text" name="password"></p>
-<p>ROL: <input type="text" name="rol"></p>
+<p>USERNAME: <input type="text" value="<?php if(isset($_GET['engine'])){ echo $_GET['engine'];}?>" name="username"></p>
+<p>EMAIL: <input type="text" name="email" value="<?php if(isset($_GET['email'])){ echo $_GET['email'];}?>"></p>
+<p>PASSWORD: <input type="text" name="password" value="<?php if(isset($_GET['pass'])){ echo $_GET['pass'];}?>"></p>
+<p>ROL: <input type="text" name="rol" value="<?php if(isset($_GET['rol'])){ echo $_GET['rol'];}?>"></p>
 <input type="submit">
 </form>
 </body>
